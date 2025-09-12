f56556bf379e229610a651c16aabf788dfb2cca2
SMVEC2025 <webdeveloper@smvec.ac.in>
2025-09-12 11:19:17 +0530

    changes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,5 +2,13 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+    {
+    auth: {
+      flowType: 'pkce',           // prefer PKCE over implicit (no hash tokens)
+      detectSessionInUrl: true,   // parse auth params on load
+      autoRefreshToken: true,
+      persistSession: true,
+    },
+  }
 );
